e77fa1c0fe74cb362f7c78823815d7c885e41cd7
alejandro <alejandroj.zavala3@gmail.com>
2023-08-11 00:42:42 -0600

    adding new tag and favicon to the index.html also removed react-helmet dependencie

--- src/App.jsx
+++ src/App.jsx
@@ -6,7 +6,7 @@ import { TaskProvider } from "./context/task-context";
 import Tasks from "./components/Tasks/Tasks";
 import bgVideoNight from "./assets/bg/44d8cc48-4db8-4be5-8530-ec86dfe871bf.webm";
 import bgVideoDay from "./assets/bg/8a4d521f-4b34-41b0-a1cb-7f30984f033d.webm";
-import { Helmet } from "react-helmet";
+import logo from "./assets/logo/97ced400-bd62-48cd-a883-f2ec8daa6fa9.ico";
 function App() {
   return (
     <>
