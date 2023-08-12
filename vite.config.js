095414636690e998536fa759aa5ba449e7e2c451
alejandro <alejandroj.zavala3@gmail.com>
2023-08-12 00:37:39 -0600

    correctly vite.config.js for deploy

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from "@vitejs/plugin-react";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "https://wmlondo.github/io/focusflow-app",
+  base: "https://wmlondo.github.io/focusflow-app",
 });
