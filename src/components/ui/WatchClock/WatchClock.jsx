1d317d175d6e74799155f626a19c936ca26fb840
alejandro <alejandroj.zavala3@gmail.com>
2023-08-29 17:29:16 -0600

    Normalize: clock time format all device

--- src/components/ui/WatchClock/WatchClock.jsx
+++ src/components/ui/WatchClock/WatchClock.jsx
@@ -5,9 +5,23 @@ const WatchClock = () => {
   const [currentTime, setCurrentTime] = useState("");
 
   useEffect(() => {
-    setCurrentTime(new Date().toLocaleTimeString());
+    setCurrentTime(
+      new Date().toLocaleTimeString("en-US", {
+        hour12: true,
+        hour: "2-digit",
+        minute: "numeric",
+        second: "numeric",
+      })
+    );
     const interval = setInterval(() => {
-      setCurrentTime(new Date().toLocaleTimeString());
+      setCurrentTime(
+        new Date().toLocaleTimeString("en-US", {
+          hour12: true,
+          hour: "2-digit",
+          minute: "numeric",
+          second: "numeric",
+        })
+      );
     }, 1000);
     return () => clearInterval(interval);
   }, [currentTime]);
